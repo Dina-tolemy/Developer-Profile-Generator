9354b8967f987a90d88f6053baaadfab63a6c1e5
Dinatolemy <dina.a.tolemy@gmail.com>
2020-01-20 12:48:12 +1100

    changing to pdfmake

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
+var fonts = {
+    Courier: {
+        normal: 'Courier',
+        bold: 'Courier-Bold',
+        italics: 'Courier-Oblique',
+        bolditalics: 'Courier-BoldOblique'
+    },
+    Helvetica: {
+        normal: 'Helvetica',
+        bold: 'Helvetica-Bold',
+        italics: 'Helvetica-Oblique',
+        bolditalics: 'Helvetica-BoldOblique'
+    },
+    Times: {
+        normal: 'Times-Roman',
+        bold: 'Times-Bold',
+        italics: 'Times-Italic',
+        bolditalics: 'Times-BoldItalic'
+    },
+    Symbol: {
+        normal: 'Symbol'
+    },
+    ZapfDingbats: {
+        normal: 'ZapfDingbats'
+    }
+};
+
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
-var pdfMake = require('pdfmake/build/pdfmake.js');
-var pdfFonts = require('pdfmake/build/vfs_fonts.js');
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
-
+var PdfPrinter = require('pdfmake');
+var printer = new PdfPrinter(fonts);
 inquirer
     .prompt({
         message: "Enter your GitHub username:",
@@ -14,19 +39,35 @@ inquirer
     .then(function ({ username }) {
         const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
         axios.get(queryUrl)
-        .then(function (res) {
-            console.log(res);
-            const repoNames = res.data.map(function (repo) {
-                return repo.name;
-            });
-            const repoNamesStr = repoNames.join("\n");
-            doc.fontSize(25).text(username, 100, 80);
-            doc.fontSize(22).text(`${username} has ${repoNames.length} projects`);
-            doc.fontSize(20).text(repoNamesStr);
-            doc.end();
-            stream.on('finish', function () {
-                iframe.src = stream.toBlobURL('resume/pdf');
-            });
+            .then(function (res) {
+               // console.log(res);
+                const repoNames = res.data.map(function (repo) {
+                    return repo.name;
+                });
+                const projectUrl = res.data.map(function (repo) {
+                    return repo.url;
+                });
+                const repoUrlStr = projectUrl.join("\n");
+                const repoNamesStr = repoNames.join("\n");
+                var docDefinition = {
+                    content: [
+                        `${username}`,
+                        '\n',
+                        `${username} has ${repoNames.length} project: `,
+                        "\n",
+                        `${repoNamesStr}`,
+                        "\n",
+                        "links to the deployed projects on git hub: ",
+                        "\n",
+                        `${repoUrlStr}`,
+                    ],
+                    defaultStyle: {
+                        font: 'Helvetica'
+                    }
+                };
+                var pdfDoc = printer.createPdfKitDocument(docDefinition);
+                pdfDoc.pipe(fs.createWriteStream('document.pdf'));
+                pdfDoc.end();
 
-        });
+            });
     });
